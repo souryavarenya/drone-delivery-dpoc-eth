bbecb7ae715285eb034f3aeb117201e01b2c478c
kzhuoxu <57842826+kzhuoxu@users.noreply.github.com>
2019-12-19 09:29:30 +0100

    Update PolicyIteration.m

--- code/PolicyIteration.m
+++ code/PolicyIteration.m
@@ -39,4 +39,54 @@ global TERMINAL_STATE_INDEX
 % IMPORTANT: You can use the global variable TERMINAL_STATE_INDEX computed
 % in the ComputeTerminalStateIndex.m file (see main.m)
 
+L = size(G,2); % number of control inputs
+converged = 0;
+
+u_opt = ones(K-1,1);
+u_opt_imp = ones(K-1,1);
+
+P_ = zeros(K-1,K-1);
+G_ = zeros(K-1,1);
+
+G(G==Inf) = 1e10;
+iter = 0;
+
+P(TERMINAL_STATE_INDEX,:,:) = [];
+P(:,TERMINAL_STATE_INDEX,:) = [];
+G(TERMINAL_STATE_INDEX,:) = [];
+
+while ~converged
+    
+    % Policy Evaluation
+    for i = 1:K-1
+        for j = 1:K-1
+            G_(i,1) = G(i,u_opt(i));
+            P_(i,j) = P(i,j,u_opt(i));
+        end
+    end
+    
+    J = (eye(K-1,K-1) - P_)\G_;
+    
+    % Policy Improvement
+    for i = 1:K-1
+        argmin = 1e10;
+        for u = 1:L
+            if argmin > G(i,u) + P(i,:,u)*J
+                u_opt_imp(i) = u;    % find optimal inputs
+                argmin = G(i,u) + P(i,:,u)*J;
+                iter = iter + 1;
+            end
+        end
+    end
+    
+    if all(u_opt_imp - u_opt < 1e-50)
+        converged = 1;
+        disp('Policy iteration converged.')
+    end
+    
+    u_opt = u_opt_imp;
+    J_opt = [J(1:TERMINAL_STATE_INDEX-1);0;J(TERMINAL_STATE_INDEX:end)];
+    u_opt_ind = [u_opt(1:TERMINAL_STATE_INDEX-1);1;u_opt(TERMINAL_STATE_INDEX:end)];
+    
+end
 end
